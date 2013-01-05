0799a300258ee07322b4c0d456b8bed413fed70d
Shanarelle <cvl_reid@hotmail.com>
2013-01-05 22:40:18 +0000

    Menu system dysfunctional - commented out. Have bad candies and lives

--- candy.py
+++ candy.py
@@ -2,6 +2,10 @@ import pygame
 import os
 import random
 
+NEWCANDY = pygame.USEREVENT + 1
+DEADCANDY = pygame.USEREVENT + 2
+BADCANDY = pygame.USEREVENT + 3
+
 def load_image(name):
     fullname = os.path.join('data', name)
     try:
@@ -12,26 +16,52 @@ def load_image(name):
     image = image.convert()
     return image, image.get_rect()
 	
+def load_sliced_sprites(w, h, filename):
+    '''
+	from shinylittlething.com
+    Specs :
+    	Master can be any height.
+    	Sprites frames width must be the same width
+    	Master width must be len(frames)*frame.width
+    Assuming your resources directory is named "data"
+    '''
+    images = []
+    master_image = pygame.image.load(os.path.join('data', filename)).convert_alpha()
+
+    master_width, master_height = master_image.get_size()
+    for i in xrange(int(master_width/w)):
+    	images.append(master_image.subsurface((i*w,0,w,h)))
+    return images
+	
 	
 class Candy(pygame.sprite.Sprite):
-	def __init__(self):
+	def __init__(self, speed, ycoord):
 		pygame.sprite.Sprite.__init__(self)
-		self.image, self.rect = load_image('fist.bmp')
+		self.image, self.rect = load_image('candy.jpg')
 		screen = pygame.display.get_surface()
 		self.x = random.randrange(screen.get_width() - self.rect.width)
 		#print self.x
-		self.y = 0
+		self.y = ycoord
+		self.speed = speed
+		self.kind = 'candy'
 		#print self.y
 		#self.rect.topleft = self.x, self.y
 	
 	def update(self):
 		''' move the candy down the screen slightly '''
-		self.y = self.y + 1
+		self.y = self.y + self.speed
 		self.rect.topleft = self.x, self.y
 		''' remove from groups if dead '''
 		if self.y >= pygame.display.get_surface().get_height():
+			pygame.event.post(pygame.event.Event(DEADCANDY, xpos=self.x, speed=self.speed, kind=self.kind))
 			self.kill()
-			
+
+class BadCandy(Candy):
+	def __init__(self, speed, ycoord):
+		Candy.__init__(self, speed, ycoord)
+		self.image, self.rect = load_image('badcandy.jpg')
+		self.kind = 'badcandy'
+		
 			
 class Omnom(pygame.sprite.Sprite):
 	#moves an omnom along the bottom of the screen, 
@@ -43,15 +73,49 @@ class Omnom(pygame.sprite.Sprite):
 		self.rect = self.image.get_rect()
 		self.rect.bottom = pygame.display.get_surface().get_height()
 		self.status = 0
+		''' for animation '''
+		self._delay = 1000 / 40
+		self._last_update = 0
+		self._frame = -1
+		# the two possible animations
+		self._images = load_sliced_sprites(75, 70, 'omnom-opening.jpg')
+		self._images2 = load_sliced_sprites(75, 70, 'omnom-nommin.jpg')
 	
 	def update(self):
-		''' move the omnom based on the mouse position '''
+		''' move the omnom along the bottom of the screen 
+		    based on the mouse position '''
 		pos = pygame.mouse.get_pos()
 		screen = pygame.display.get_surface()
 		if pos[0] < (screen.get_width()-self.rect.width):
 			self.rect.left = pos[0]
 		else:
 			self.rect.right = screen.get_width()
+		''' do some animation '''
+		t = pygame.time.get_ticks()
+		if self.status != 0:
+			if t - self._last_update > self._delay:
+				self._frame += 1
+				#run through opening mouth sequence
+				if self.status == 1:
+					if self._frame < len(self._images):
+						self.image = self._images[self._frame]
+						self._last_update = t
+						'''
+					else:	#gone through a single round of animation
+						self._frame = -1
+						self.status = 0
+						self.image = pygame.image.load('omnom.jpg').convert()
+						'''
+				# run through closing mouth sequence
+				else:
+					if self._frame < len(self._images2):
+						self.image = self._images2[self._frame]
+						self._last_update = t
+					else:	#gone through a single round of animation
+						self._frame = -1
+						self.status = 0
+						self.image = pygame.image.load('omnom.jpg').convert()
+	
 	
 	def ready(self, candylist):
 		bigger = pygame.sprite.collide_rect_ratio(1.5)
@@ -59,13 +123,48 @@ class Omnom(pygame.sprite.Sprite):
 		if len(close_enough) > 0:	#change image
 			if self.status != 1:
 				self.status = 1
-				self.image = pygame.image.load('omnom-ready.jpg').convert()
+				self._frame = -1
 			collision = pygame.sprite.spritecollide(self, candylist, True)
 			if len(collision) > 0:	#eaten candy
 				self.status = 2
-				self.image = pygame.image.load('omnom-eaten.jpg').convert()
+				self._frame = -1
 			return collision
 		else:
+			if self.status != 0:
+				self.status = 0
+				self.image = pygame.image.load('omnom.jpg').convert()
 			return []
+		
 			
+class Explosion(pygame.sprite.Sprite):
+	def __init__(self, xpos):
+		pygame.sprite.Sprite.__init__(self)
+		self.image, self.rect = load_image('explosion.jpg')
+		screen = pygame.display.get_surface()
+		self.x = xpos
+		self.rect.bottomleft = (self.x, screen.get_height())
+		self.ticker = 0
+	
+	def update(self):
+		self.ticker += 1
+		if self.ticker > 15:
+			self.kill()
+			
+			
+class Minus(pygame.sprite.Sprite):
+	def __init__(self, xpos):
+		pygame.sprite.Sprite.__init__(self)
+		self.image, self.rect = load_image('minus.jpg')
+		screen = pygame.display.get_surface()
+		if xpos > 20:
+			self.x = xpos
+		else:
+			self.x = 20
+		self.rect.midbottom = (self.x, screen.get_height()-40)
+		self.ticker = 0
+	
+	def update(self):
+		self.ticker += 1
+		if self.ticker > 15:
+			self.kill()
 			
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
+import MenuSystem
+import os
 import candy
-
-
+import random
 
 pygame.init()
-screen = pygame.display.set_mode((480, 320))
-pygame.display.set_caption('Raining Candy      0')
+pygame.mixer.init()
+screen = pygame.display.set_mode((560, 320))
+pygame.display.set_caption('Raining Candy')
 pygame.mouse.set_visible(0)
 
 background = pygame.Surface(screen.get_size()).convert()
@@ -13,38 +15,158 @@ background.fill((250, 250, 250))
 screen.blit(background, (0, 0))
 pygame.display.flip()
 
+'''
+#set up menu
+MenuSystem.init()
+# create the menus and submenus
+sound_options = MenuSystem.Menu('Sounds', ('Toggle Background Music', 'Toggle Sound Effects'))
+main_menu = MenuSystem.Menu('Menu', ('Options')) #'New Game    (F4)', 'Pause    (p)', 'High Scores', 'Quit'))
+bar = MenuSystem.MenuBar()
+bar.set((main_menu))
+static = bar.lineheigth
+pygame.display.update(bar)
+ms = MenuSystem.MenuSystem()
+'''
+static = 14
+
+#set up omnom character
 character = candy.Omnom()
 allsprites = pygame.sprite.RenderPlain((character))	#group
 clock = pygame.time.Clock()
 
+#set up game tracking variables and sound effects
 candylist = pygame.sprite.RenderPlain()
+badlist = pygame.sprite.RenderPlain()
 cont = True
-round = 0
 score = 0
+misses = 0
+missed_sound = pygame.mixer.Sound(os.path.join('data', 'candy-missed.wav'))
+level_up_sound = pygame.mixer.Sound(os.path.join('data', 'level-up.wav'))
+level_up_sound.set_volume(0.1)
+background_music = pygame.mixer.Sound(os.path.join('data', 'background-music.ogg'))
+background_music.set_volume(0.05)
+levelthreshold = 5
+level = 1
+lives = 3
+candyGen = pygame.time.set_timer(candy.NEWCANDY, 1000)
+badCandyGen = pygame.time.set_timer(candy.BADCANDY, 5000)
 
-while cont:
+# initialise a font module to display things to the screen
+font = pygame.font.Font(None, 42)
+text = font.render('Begin!!', True, (0,0,255), (255,255,255))
+textRect = text.get_rect()
+textRect.centerx = screen.get_rect().centerx
+textRect.centery = screen.get_rect().centery
+
+screen.blit(text, textRect)
+pygame.display.flip()
+pygame.time.wait(500)
+background_music.play(loops=-1)
+
+'''
+ev = event.wait()
+pygame.display.update(bar.update(ev))
+if bar.choice:
+	print(bar.choice)
+	print(bar.choice_label)
+	print(bar.choice_index)
+pygame.display.flip()
+'''
+
+while cont: 
 	clock.tick(30)
-	round += 1
+	screen.blit(background, (0, static))
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			cont = False
+			background_music.stop()
 		elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
 			cont = False
+			background_music.stop()
+		elif event.type == pygame.KEYDOWN and event.key == pygame.K_p:
+			paused = True
+			while paused:
+				event = pygame.event.wait()
+				if event.type == pygame.KEYDOWN and event.key == pygame.K_p:
+					paused = False
+		elif event.type == candy.NEWCANDY:
+			candylist.add(candy.Candy(random.randrange(1, 6), static))
+		elif event.type == candy.BADCANDY:
+			badlist.add(candy.BadCandy(random.randrange(1, 6), static))
+		elif event.type == candy.DEADCANDY:
+			misses += 1
+			# some penalties for a miss based on current level
+			# if level is above 5 then missing a slow candy deducts a point
+			if event.kind == 'badcandy':
+				pass
+			else:
+				if level > 1 and event.speed < 3:
+					score -= 1
+					allsprites.add(candy.Minus(event.xpos))
+				
+				allsprites.add(candy.Explosion(event.xpos))	
+				missed_sound.play()
+			pygame.display.set_caption('Raining Candy      Level: ' + repr(level) + ',  Score: ' + repr(score) + ', Candies missed: ' + repr(misses) + ', Lives:' + repr(lives))
+		'''else:	# will only accept mouse motion events
+			if ms:
+				pygame.display.update(ms.update(event))
+				if ms.choice:
+					print(ms.choice)
+					print(ms.choice_label)
+					print(ms.choice_index)
+			#else:
+			pygame.display.update(bar.update(event))
+			if bar.choice:
+				print(bar.choice)
+				print(bar.choice_label)
+				print(bar.choice_index)
+	#bar.draw()	'''
 	allsprites.update()
-	print allsprites
-	if round % 100 == 0:
-		#candy = candy.Candy()
-		candylist.add(candy.Candy())
-		allsprites.add(candylist)
+	candylist.update()
+	badlist.update()
+	#print allsprites
 	if len(candylist) > 0:
 		result = character.ready(candylist)
-		# should do an action if this was true - start a timer
+		badresult = character.ready(badlist)
 		if len(result) > 0:	#THERE WAS A COLLISION
 			score += 1
-			pygame.display.set_caption('Raining Candy     ' + repr(score))
-	screen.blit(background, (0, 0))
+		if len(badresult) > 0:
+			lives -= 1
+		pygame.display.set_caption('Raining Candy      Level: ' + repr(level) + ',  Score: ' + repr(score) + ', Candies missed: ' + repr(misses) + ', Lives:' + repr(lives))		
 	allsprites.draw(screen)
+	candylist.draw(screen)
+	badlist.draw(screen)
 	pygame.display.flip()
+	
+	# if the score is sufficient, advance to the next level and display some congratulatory text
+	if score >= levelthreshold:
+		levelthreshold = levelthreshold * 3
+		level += 1
+		pygame.display.set_caption('Raining Candy      Level: ' + repr(level) + ',  Score: ' + repr(score) + ', Candies missed: ' + repr(misses) + ', Lives:' + repr(lives))
+		level_up_sound.play()
+		text = font.render('Level Up!', True, (0, 0, 255))
+		textRect = text.get_rect()
+		textRect.centerx = screen.get_rect().centerx
+		textRect.centery = screen.get_rect().centery
+		screen.blit(text, textRect)
+		pygame.display.flip()
+		pygame.time.wait(800)
+		
+	if lives < 1:
+		screen.blit(background, (0, static))
+		text = font.render('Game Over', True, (0, 0, 255))
+		textRect = text.get_rect()
+		textRect.midbottom = (screen.get_rect().centerx, screen.get_rect().centery+2)
+		screen.blit(text, textRect)
+		font_small = pygame.font.Font(None, 24)
+		text1 = font.render('Score: ' + repr(score), True, (0, 0, 255))
+		text1Rect = text.get_rect()
+		text1Rect.midtop = (screen.get_rect().centerx, screen.get_rect().centery-2)
+		screen.blit(text1, text1Rect)
+		pygame.display.flip()
+		pygame.time.wait(800)
+		pygame.event.post(pygame.event.Event(pygame.QUIT))
+
 		
 		
 		
